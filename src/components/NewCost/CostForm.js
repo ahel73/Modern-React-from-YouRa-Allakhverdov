89d6fbce5ab21ce4a9f5d46d168feadaeb4c2a37
Нагаев Алексей <nag495@yandex.ru>
2023-08-30 09:52:44 +0300

    3-39/41 Обработка Отправки Формы - сохраняем данные в переменную при submit

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import './CostForm.css';
 
 const CostForm = () => {
-    const [name, setName] = useState("");
-    const [amount, setAmount] = useState("");
-    const [date, setDate] = useState("");
+    const [inputName, setInputName] = useState("");
+    const [inputAmount, setInpuAmount] = useState("");
+    const [inputDate, setInpuDate] = useState("");
 
     // const [userInput, setUserInput] = useState({
     //     name: "",
@@ -13,7 +13,7 @@ const CostForm = () => {
     // });
 
     const nameChangeHandler = (event) => {
-        setName(event.target.value);
+        setInputName(event.target.value);
         // setUserInput({
         //     ...userInput,
         //     name: event.target.value,
@@ -21,23 +21,31 @@ const CostForm = () => {
     };
 
     const amountChangeHandler = (event) => {
-        setAmount(event.target.value);
+        setInpuAmount(event.target.value);
         // setUserInput({
         //     ...userInput,
         //     amount: event.target.value,
         // });
     };
 
-  const dateChangeHandler = (event) => {
-        setDate(event.target.value);
-        // setUserInput({
-        //     ...userInput,
-        //     date: event.target.value,
-        // });
+    const dateChangeHandler = (event) => {
+            setInpuDate(event.target.value);
     };
 
+    const submithandler = (event) => {
+        event.preventDefault();
+        
+        const costData = {
+            name: inputName,
+            amount: inputAmount,
+            date: new Date(inputDate),
+        }
+
+        console.log('costData', costData)
+    }
+
     return (
-        <form>
+        <form onSubmit={submithandler}>
             <div className="new-cost__controls">
                 <div className="new-cost__control">
                     <label>Название</label>
