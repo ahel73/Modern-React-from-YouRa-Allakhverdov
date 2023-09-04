15642ee6add917063fd60488882da121ff36b93b
Нагаев Алексей <nag495@yandex.ru>
2023-09-04 10:16:28 +0300

    3-42|44-45|47 Подъём состояния, компоненты с состоянием и нет

--- src/components/Cost/CostItem.js
+++ src/components/Cost/CostItem.js
@@ -1,25 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from '../Ui/Card';
 import CostDate from './CostDate';
 import "./CostItem.css";
 
-export default function CostItem(props) {
-    const [description, setDescription] = useState(props.description);
-    
-    const changeDescriptionHandler = () => {
-        setDescription('туц еуче ');
-    }
-    
+export default function CostItem(props) { 
     return (        
         <Card className='cost-item'>
             <CostDate date={props.date} />
             <div className='cost-item__description'>
                 <h2>
-                    {description}
+                    {props.description}
                 </h2>
                 <div className='cost-item__price'>{ props.amount}</div>
             </div>
-            <button onClick={changeDescriptionHandler}>Изменить описание</button>
         </Card>
     )
 }
--- src/components/Cost/Costs.js
+++ src/components/Cost/Costs.js
@@ -1,11 +1,19 @@
 import Card from '../Ui/Card';
 import CostItem from './CostItem';
 import './Costs.css';
+import CostsFilter from "./CostsFilter";
+import React, { useState } from "react";
 
 export default function Costs(prop) {
+    const [selectedYear, setSelectedYear] = useState("2021");
+
+    const yearChangeHandler = (year) => {
+        setSelectedYear(year);
+    };
     
     return (
         <Card className='costs'>
+            <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
             {
                 prop.costs.map((el) => (
                     <CostItem
